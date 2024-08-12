443480eccb195a68a830137f2d3ed728edfc38e0
yukik8 <andres8.yuuki@gmail.com>
2024-08-12 12:16:53 +0900

    Initial Commit

---
+++ KpopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KpopApp
+//
+//  Created by Yuki Kikuchi on 2024/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KpopApp/KpopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KpopAppApp.swift
+//  KpopApp
+//
+//  Created by Yuki Kikuchi on 2024/08/12.
+//
+
+import SwiftUI
+
+@main
+struct KpopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
